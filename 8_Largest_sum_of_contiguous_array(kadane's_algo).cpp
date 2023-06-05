a9be1eb84aadb5094e95751bf59a436f1f3a4b6f
Rohan Kumar Choudhary <rohanchoudharynitp@gmail.com>
2023-06-05 19:52:56 +0530

    Create 8_Largest_sum_of_contiguous_array(kadane's_algo).cpp

---
+++ 8_Largest_sum_of_contiguous_array(kadane's_algo).cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        long long cursum=0;
+        long long maxsum=INT_MIN;
+        for(int i=0;i<n;i++)
+        {   
+            cursum+=arr[i];
+            if(cursum>maxsum)
+            {
+                maxsum=cursum;
+            }
+            if(cursum<0)
+            {
+                cursum=0;
+            }
+        }
+        return maxsum;
+    }
+};
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
